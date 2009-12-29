68f3cfb1fbf9695f26ca2cfcdcb53c989f3bb209
Victor Ek <victor.ek@gmail.com>
2009-12-29 14:55:06 +0100

    static pages done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
+  #@@ = class variable	exist in whole PagesController, not in erb
+  @@sitetitle = "Railstwitterclone - "
+  
   def home
+  	#@ = instance variable, exist only in home
+  	@title = @@sitetitle + "Home"
   end
 
   def contact
+  	@title = @@sitetitle + "Contact"
+  end
+  
+  def about
+  	@title = @@sitetitle + "About"
   end
 
 end
